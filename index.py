16d0bb5ec27b316d5eeff2c070bd12eb4a8e4b59
Breadman <jordanjjana@gmail.com>
2025-07-10 22:53:50 +0100

    Finished the actual generator then i'll add more stuff including an actual UI later

--- index.py
+++ index.py
@@ -2,19 +2,29 @@ import random
 import string
 import tkinter
 m = tkinter.Tk()
-def randstring(size=15, chars=string.ascii_lowercase + string.digits + string.ascii_uppercase):
+jms = 0
+def change(val):
+    global jms
+    jms = int(val)
+w2 = tkinter.Scale(m, from_=1, to=30, orient=tkinter.HORIZONTAL, command=change, length=300)
+w2.place(x=100, y=135)
+def randstring(size=jms, chars=string.ascii_letters + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
-
-def create_message():
-    global genm2
-    genm2 = tkinter.Message(m, text=randstring(), width=105, wrap=None)
-    genm2.pack()
-
-genm = randstring()
-genm2 = tkinter.Message(m, text=genm, width=105 ,wrap=None)
-genm2.config(wrap=None)
-genm2.place(x=20, y=-55)
-genm2.pack()
-button = tkinter.Button(m, text='Generate', width=25, command=create_message)
-button.pack()
+label = tkinter.Label(m, text=randstring(jms))
+label.place(x=165, y=205)
+def generate_new_string():
+    new_str = randstring(jms)
+    label.config(text=new_str)
+def copy_to_clipboard():
+    m.clipboard_clear()
+    m.clipboard_append(
+        label.cget("text").strip()
+    )
+    m.update()
+copy_button = tkinter.Button(m, text="Copy to Clipboard", command=copy_to_clipboard)
+copy_button.place(x=100, y=250)
+button = tkinter.Button(m, text="Generate", command=generate_new_string)
+button.place(x=100, y=200)
+m.geometry("500x500")
+m.title("Password Generator")
 m.mainloop()
